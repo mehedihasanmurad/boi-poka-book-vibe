ecc47a9476ff3e80cb493e7a7228ee99e4b6a5af
Mehedi Hasan Murad <mehedihasna500422@gamil.com>
2025-07-28 13:22:16 +0600

    navbar added

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="text-4xl">Page Not Found</h2>
+            <p className='text-xl'>status 404</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
